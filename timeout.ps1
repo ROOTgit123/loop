e8150b8df447ea57bb0d0382e578b38c55935143
ROOTgit123 <161525429+ROOTgit123@users.noreply.github.com>
2024-06-16 11:09:58 +0100

    Update timeout.ps1

--- timeout.ps1
+++ timeout.ps1
@@ -1,6 +1,15 @@
-$i = 99999
-do {
-    Write-Host $i
-    Start-Sleep -Seconds 99999
-    $i--
-} while ($i -gt 0)
+$timer = New-Object Timers.Timer
+$timer.Interval = 60000 # 60 seconds
+$timer.AutoReset = $true
+$timer.add_Elapsed({ Write-Host "Keeping the session alive..." })
+
+$timer.Start()
+Write-Host "Timer started. Press Ctrl+C to stop."
+try {
+    while ($true) {
+        Start-Sleep -Seconds 60000
+    }
+} finally {
+    $timer.Stop()
+    $timer.Dispose()
+}
